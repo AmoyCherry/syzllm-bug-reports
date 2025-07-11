712458498afc65e5ee577d440ccae624d4221b93
Vincent <31067272+AmoyCherry@users.noreply.github.com>
2025-07-11 12:09:29 -0400

    add rc5 bugs

--- filter_bugs.py
+++ filter_bugs.py
@@ -5,21 +5,30 @@ filtered_description = ["WARNING", "INFO: ", "possible deadlock", "unregister_ne
 
 def remove_warning_subfolders(root_folder):
     for subdir, dirs, files in os.walk(root_folder):
-        # Only look at immediate subfolders
-        for d in dirs:
-            desc_path = os.path.join(subdir, d, "description")
+        for d in dirs[:]:  # Create a copy of dirs to avoid modifying while iterating
+            subfolder_path = os.path.join(subdir, d)
+            # Check if subfolder still exists (in case it was deleted)
+            if not os.path.exists(subfolder_path):
+                continue
+
+            # Check description file
+            desc_path = os.path.join(subfolder_path, "description")
+            repro_path = os.path.join(subfolder_path, "repro.cprog")
+
+            # Delete if description contains filtered strings or repro.cprog is missing
+            should_delete = False
             if os.path.isfile(desc_path):
                 with open(desc_path, "r") as f:
                     content = f.read()
                 if any(s in content for s in filtered_description):
-                    shutil.rmtree(os.path.join(subdir, d))
-            
-            repro_path = os.path.join(subdir, d, "repro.cprog")
+                    should_delete = True
             if not os.path.isfile(repro_path):
-                # If repro.cprog does not exist, remove the subfolder
-                shutil.rmtree(os.path.join(subdir, d))
+                should_delete = True
 
-        # Prevent descending into subfolders we might delete
+            if should_delete:
+                shutil.rmtree(subfolder_path)
+
+        # Prevent descending into subfolders
         break
 
 if __name__ == "__main__":
